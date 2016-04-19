3c63d7ac963f2baf2f314d8ddf91922c65b1bd7c
Ed Schouten <ed@nuxi.nl>
2016-04-19 17:01:42 +0200

    Add a formula for installing cloudabi-run.

---
+++ cloudabi-utils.rb
@@ -0,0 +1,15 @@
+class CloudabiUtils < Formula
+  desc "Utilities for starting CloudABI programs"
+  homepage "https://github.com/NuxiNL/cloudabi-utils"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.9.tar.gz"
+  sha256 "947346fc15b14bd338613c292897ed9e597d9e1ed5469b64c92962bb9c90635d"
+
+  depends_on "cmake" => :build
+  depends_on "cloudabi" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make"
+    system "make", "install"
+  end
+end
