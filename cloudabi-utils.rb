69cca17e9ac911aecf0daa28c62a3bde1787c78a
Ed Schouten <ed@nuxi.nl>
2016-04-19 21:13:31 +0200

    Add libyaml as a dependency.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -4,8 +4,9 @@ class CloudabiUtils < Formula
   url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.9.tar.gz"
   sha256 "947346fc15b14bd338613c292897ed9e597d9e1ed5469b64c92962bb9c90635d"
 
-  depends_on "cmake" => :build
   depends_on "cloudabi" => :build
+  depends_on "cmake" => :build
+  depends_on "libyaml"
 
   def install
     system "cmake", ".", *std_cmake_args
