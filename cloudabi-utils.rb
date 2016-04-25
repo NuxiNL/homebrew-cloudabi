0f8db8d3629af1430e50b060c54140e43eb8f52b
Ed Schouten <ed@nuxi.nl>
2016-04-25 20:06:42 +0200

    Upgrade cloudabi-utils to the latest version.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -1,8 +1,8 @@
 class CloudabiUtils < Formula
   desc "Utilities for starting CloudABI programs"
   homepage "https://github.com/NuxiNL/cloudabi-utils"
-  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.9.tar.gz"
-  sha256 "947346fc15b14bd338613c292897ed9e597d9e1ed5469b64c92962bb9c90635d"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.11.tar.gz"
+  sha256 "7f52764977a1e772c7ecf9011e1d0f946fedcd9e1fe4d791dfd23bd40f77cd20"
 
   depends_on "cloudabi" => :build
   depends_on "cmake" => :build
