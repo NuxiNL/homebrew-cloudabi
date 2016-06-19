45800b81b69291682ac6c1498705b9dc53b78d43
Ed Schouten <ed@nuxi.nl>
2016-06-19 08:39:29 +0200

    Sync in latest formulae from the CloudABI Ports Collection.

--- aarch64-unknown-cloudabi-cloudlibc.rb
+++ aarch64-unknown-cloudabi-cloudlibc.rb
@@ -3,14 +3,14 @@ class Aarch64UnknownCloudabiCloudlibc < Formula
   homepage "https://github.com/NuxiNL/cloudlibc"
   url "http://this.package.cannot.be.built.from.source/"
   version "0.38"
-  revision 1
+  revision 2
   depends_on "aarch64-unknown-cloudabi-cloudabi"
   depends_on "aarch64-unknown-cloudabi-compiler-rt"
 
   bottle do
     root_url "https://nuxi.nl/distfiles/cloudabi-ports/homebrew/links"
-    sha256 "5cb1c789709281ddfb3408a61a42a74fedb6c336c51faa385c7aa6c4cfff6ab5" => :el_capitan
-    sha256 "5cb1c789709281ddfb3408a61a42a74fedb6c336c51faa385c7aa6c4cfff6ab5" => :mavericks
-    sha256 "5cb1c789709281ddfb3408a61a42a74fedb6c336c51faa385c7aa6c4cfff6ab5" => :yosemite
+    sha256 "3b08a53caad08f8db8abe0221d1813f785e8fcee4da485e32abf3f7d19deb0da" => :el_capitan
+    sha256 "3b08a53caad08f8db8abe0221d1813f785e8fcee4da485e32abf3f7d19deb0da" => :mavericks
+    sha256 "3b08a53caad08f8db8abe0221d1813f785e8fcee4da485e32abf3f7d19deb0da" => :yosemite
   end
 end
--- x86_64-unknown-cloudabi-cloudlibc.rb
+++ x86_64-unknown-cloudabi-cloudlibc.rb
@@ -3,14 +3,14 @@ class X8664UnknownCloudabiCloudlibc < Formula
   homepage "https://github.com/NuxiNL/cloudlibc"
   url "http://this.package.cannot.be.built.from.source/"
   version "0.38"
-  revision 1
+  revision 2
   depends_on "x86_64-unknown-cloudabi-cloudabi"
   depends_on "x86_64-unknown-cloudabi-compiler-rt"
 
   bottle do
     root_url "https://nuxi.nl/distfiles/cloudabi-ports/homebrew/links"
-    sha256 "718cf6ccb501338c8f5a26fa03499e0963b0bdabb457797cb7106cb38607cb82" => :el_capitan
-    sha256 "718cf6ccb501338c8f5a26fa03499e0963b0bdabb457797cb7106cb38607cb82" => :mavericks
-    sha256 "718cf6ccb501338c8f5a26fa03499e0963b0bdabb457797cb7106cb38607cb82" => :yosemite
+    sha256 "975e33243954b3151a18c0e8ec60753661016cf78e1b58971dda34e375d24cf4" => :el_capitan
+    sha256 "975e33243954b3151a18c0e8ec60753661016cf78e1b58971dda34e375d24cf4" => :mavericks
+    sha256 "975e33243954b3151a18c0e8ec60753661016cf78e1b58971dda34e375d24cf4" => :yosemite
   end
 end
