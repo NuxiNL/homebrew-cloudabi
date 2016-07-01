aa81afdc197bde18d58b3d75794c64755db3c3c8
Ed Schouten <ed@nuxi.nl>
2016-07-01 23:32:36 +0200

    Sync in latest formulae from the CloudABI Ports Collection.

--- x86_64-unknown-cloudabi-cloudlibc.rb
+++ x86_64-unknown-cloudabi-cloudlibc.rb
@@ -2,15 +2,15 @@ class X8664UnknownCloudabiCloudlibc < Formula
   desc "cloudlibc for x86_64-unknown-cloudabi"
   homepage "https://github.com/NuxiNL/cloudlibc"
   url "http://this.package.cannot.be.built.from.source/"
-  version "0.40"
+  version "0.41"
   revision 1
   depends_on "x86_64-unknown-cloudabi-cloudabi"
   depends_on "x86_64-unknown-cloudabi-compiler-rt"
 
   bottle do
     root_url "https://nuxi.nl/distfiles/cloudabi-ports/homebrew/links"
-    sha256 "b35afc884d61e45fb0f3dc03c4a6c9e7e167047317360ac4ae64d785811c29d5" => :el_capitan
-    sha256 "b35afc884d61e45fb0f3dc03c4a6c9e7e167047317360ac4ae64d785811c29d5" => :mavericks
-    sha256 "b35afc884d61e45fb0f3dc03c4a6c9e7e167047317360ac4ae64d785811c29d5" => :yosemite
+    sha256 "9a72edd9fe7793d570c3d6562e60068c36ca7cd6fa6f6514ab68a29bc58aaa0f" => :el_capitan
+    sha256 "9a72edd9fe7793d570c3d6562e60068c36ca7cd6fa6f6514ab68a29bc58aaa0f" => :mavericks
+    sha256 "9a72edd9fe7793d570c3d6562e60068c36ca7cd6fa6f6514ab68a29bc58aaa0f" => :yosemite
   end
 end
