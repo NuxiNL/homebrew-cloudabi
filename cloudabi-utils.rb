e37e3134dfaa33b8ecd46d7b69017336f00a1b58
Ed Schouten <ed@nuxi.nl>
2016-07-14 23:22:54 +0200

    Upgrade cloudabi-utils to the latest upstream version.
    On OS X, this new release fixes an issue with
    clock_gettime(CLOCK_PROCESS_CPUTIME_ID) returning a time value that's
    off by a factor 10000.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -1,8 +1,8 @@
 class CloudabiUtils < Formula
   desc "Utilities for starting CloudABI programs"
   homepage "https://github.com/NuxiNL/cloudabi-utils"
-  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.11.tar.gz"
-  sha256 "7f52764977a1e772c7ecf9011e1d0f946fedcd9e1fe4d791dfd23bd40f77cd20"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.12.tar.gz"
+  sha256 "18388414424d1f01f0f68f08a6f2afbdc110bc9c700e69c4e8227fb0a0a14c7a"
 
   depends_on "cloudabi" => :build
   depends_on "cmake" => :build
