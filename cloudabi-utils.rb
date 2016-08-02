8d2c48539fa6f90c5b36230c72e06ed98f83d580
Ed Schouten <ed@nuxi.nl>
2016-08-02 19:23:15 +0200

    Upgrade cloudabi-utils to version 0.14.
    This version now has proper support for passing in timestamp and binary
    objects. It also contains an important bug fix for IPv6 support.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -1,8 +1,8 @@
 class CloudabiUtils < Formula
   desc "Utilities for starting CloudABI programs"
   homepage "https://github.com/NuxiNL/cloudabi-utils"
-  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.13.tar.gz"
-  sha256 "1c9be1cc542180dddf8ff840fca638a7f67b7847a5eacde45db5d59118f2c30b"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.14.tar.gz"
+  sha256 "21c23c39620f98b5994a42306d34b05e0c1e172164b4f85cd5f28fab0fa9077b"
 
   depends_on "cloudabi" => :build
   depends_on "cmake" => :build
