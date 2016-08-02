15728b2e8b3045a7ef66319502e072d4f5695eee
Ed Schouten <ed@nuxi.nl>
2016-08-02 14:18:45 +0200

    Update to the latest version.
    This version of cloudabi-run has improved YAML parsing logic, so it can
    now automatically infer the typing of unquoted entries.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -1,8 +1,8 @@
 class CloudabiUtils < Formula
   desc "Utilities for starting CloudABI programs"
   homepage "https://github.com/NuxiNL/cloudabi-utils"
-  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.12.tar.gz"
-  sha256 "18388414424d1f01f0f68f08a6f2afbdc110bc9c700e69c4e8227fb0a0a14c7a"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.13.tar.gz"
+  sha256 "1c9be1cc542180dddf8ff840fca638a7f67b7847a5eacde45db5d59118f2c30b"
 
   depends_on "cloudabi" => :build
   depends_on "cmake" => :build
