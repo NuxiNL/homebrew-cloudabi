3dbb33735dc8ee894dea0d22ab160201ed49f53b
Ed Schouten <ed@nuxi.nl>
2017-01-17 07:30:31 +0100

    Upgrade to the latest upstream releases of CloudABI components.

--- cloudabi-utils.rb
+++ cloudabi-utils.rb
@@ -1,8 +1,8 @@
 class CloudabiUtils < Formula
   desc "Utilities for starting CloudABI programs"
   homepage "https://github.com/NuxiNL/cloudabi-utils"
-  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.18.tar.gz"
-  sha256 "2aaf229ee09ec6ec42102bc36a28a5f18af5a278156e4e4feea36369b1ae621d"
+  url "https://github.com/NuxiNL/cloudabi-utils/archive/v0.20.tar.gz"
+  sha256 "51b5c25ca4a7beea89325443b8f253a77a6f4cf66f6a15ce87ac3fd7057f7b0e"
 
   depends_on "cloudabi" => :build
   depends_on "cmake" => :build
--- cloudabi.rb
+++ cloudabi.rb
@@ -1,8 +1,8 @@
 class Cloudabi < Formula
   desc "Definitions for the CloudABI data types and system calls"
   homepage "https://github.com/NuxiNL/cloudabi"
-  url "https://github.com/NuxiNL/cloudabi/archive/v0.8.tar.gz"
-  sha256 "dcde2eefad9ba3a56063e0c8af369f806ff16671a1a54d6a7d1d506031263ac8"
+  url "https://github.com/NuxiNL/cloudabi/archive/v0.9.tar.gz"
+  sha256 "88f3d5e374f4b605c26ab09c9e5396ae573e1a634a642f76b9b626e1d4c0f78a"
 
   def install
     include.install Dir["headers/*"]
