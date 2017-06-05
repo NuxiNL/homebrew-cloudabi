2d3b48a7f61da16748c6956581ae8f8914bac9a8
Ed Schouten <ed@nuxi.nl>
2017-06-05 18:19:45 +0200

    Upgrade Argdata to the latest version.

--- argdata.rb
+++ argdata.rb
@@ -1,8 +1,8 @@
 class Argdata < Formula
   desc "Binary serialisation format inspired by YAML, MessagePack and nvlists"
   homepage "https://github.com/NuxiNL/argdata"
-  url "https://github.com/NuxiNL/argdata/archive/v0.1.tar.gz"
-  sha256 "1fd26745e72382b9e0eda042154b3219682de980f6cdd15f574f9d4288b7df79"
+  url "https://github.com/NuxiNL/argdata/archive/v0.3.tar.gz"
+  sha256 "12e3cc43074e2d7b22a4ae81e7262c932a4b8144b5b03f59578c00aa8aea16f4"
 
   depends_on "cmake" => :build
 
