8999efe7994f96d50480ed563bf6299006f9192b
Ed Schouten <ed@nuxi.nl>
2017-08-30 20:58:43 +0200

    Add a formula for yaml2argdata.

--- yaml2argdata.rb
+++ yaml2argdata.rb
@@ -8,13 +8,6 @@ class Yaml2argdata < Formula
   depends_on "yaml-cpp"
 
   def install
-    inreplace Dir[buildpath/"yaml2argdata/*"] do |s|
-      s.gsub! "<optional>", "<experimental/optional>", false
-      s.gsub! "std::optional", "std::experimental::optional", false
-      s.gsub! "<string_view>", "<experimental/string_view>", false
-      s.gsub! "std::string_view", "std::experimental::string_view", false
-    end
-
     include.install Dir["yaml2argdata"]
   end
 end
